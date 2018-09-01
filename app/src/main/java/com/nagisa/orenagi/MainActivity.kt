43412ca2a40f3dccf40c06a566067bb0343b9b85
Kouta Takamatsu <bb1016181@gmail.com>
2018-09-01 18:39:55 +0900

    SwipeRefreshの追加

--- app/src/main/java/com/nagisa/orenagi/MainActivity.kt
+++ app/src/main/java/com/nagisa/orenagi/MainActivity.kt
@@ -2,11 +2,22 @@ package com.nagisa.orenagi
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        SwipeRefresh.setOnRefreshListener({
+            // 引っ張って離した時に呼ばれます。
+
+            //↓これないと永遠とぐるぐるします
+            if (SwipeRefresh.isRefreshing()) {
+                SwipeRefresh.setRefreshing(false);
+            }
+        })
     }
 }
